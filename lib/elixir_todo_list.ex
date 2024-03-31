a61c53d82244335dc9e6ca0648006d22bad2aac7
venir.dev <luca@venir.dev>
2024-03-31 22:58:56 +0200

    testing, testing. one two. testing.
    dyalizer up n running

--- lib/elixir_todo_list.ex
+++ lib/elixir_todo_list.ex
@@ -12,7 +12,15 @@ defmodule ElixirTodoList do
       :world
 
   """
-  def hello do
-    :world
+  @spec hello(number()) :: {:world, number()}
+  def hello(int) do
+    {:world, int + 1}
+  end
+
+  def example() do
+    case hello("world") do
+      {:world, _} -> IO.puts("Hello world")
+      _ -> IO.puts("Hello")
+    end
   end
 end
