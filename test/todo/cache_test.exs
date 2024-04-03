525e4a436e0f73cf7994d1f27ab22e2d995f57bc
venir.dev <luca@venir.dev>
2024-04-03 19:03:46 +0200

    pattern-matching-tests are so cool

--- test/todo/cache_test.exs
+++ test/todo/cache_test.exs
@@ -8,4 +8,16 @@ defmodule Todo.CacheTest do
     assert bob_pid != Todo.Cache.get(cache, "alice")
     assert bob_pid == Todo.Cache.get(cache, "bob")
   end
+
+  test "add" do
+    {:ok, cache} = Todo.Cache.start()
+    alice = Todo.Cache.get(cache, "alice")
+    date = ~D[2021-01-01]
+    task = "Alice's task"
+
+    Todo.Server.add(alice, %{date: date, title: task})
+    entries = Todo.Server.entries(alice, date)
+
+    assert [%{date: date, title: task}] = entries
+  end
 end
