04f952e46901b1e5e8dc25686bcf55727ef43416
venir.dev <luca@venir.dev>
2024-04-05 17:12:44 +0200

    fine grained supervision on db workers

--- lib/todo/db.ex
+++ lib/todo/db.ex
@@ -1,46 +1,43 @@
 defmodule Todo.Db do
-  use GenServer
+  use Supervisor
   @folder "./db"
+  @pool_size 3
 
-  def start_link(_, opts \\ [amount: 3]) do
-    GenServer.start_link(__MODULE__, opts[:amount], name: __MODULE__)
+  def start_link(_) do
+    Supervisor.start_link(__MODULE__, @pool_size, name: __MODULE__)
   end
 
   def put(key, value) do
-    worker = GenServer.call(__MODULE__, {:choose, key})
-    Todo.DbWorker.put(worker, key, value)
+    key
+    |> choose_worker()
+    |> Todo.DbWorker.put(key, value)
   end
 
   def get(key) do
-    worker = GenServer.call(__MODULE__, {:choose, key})
-    Todo.DbWorker.get(worker, key)
+    key
+    |> choose_worker()
+    |> Todo.DbWorker.get(key)
   end
 
-  def clear do
-    File.rm_rf!(@folder)
-  end
-
-  def init(pool_size) do
-    IO.puts("Starting Todo.Db")
+  @impl Supervisor
+  def init(_) do
+    IO.puts("Starting DB with pool size #{@pool_size}")
     File.mkdir_p!(@folder)
-
-    workers =
-      for i <- 0..(pool_size - 1), into: %{} do
-        {:ok, worker} = Todo.DbWorker.start_link({@folder, i})
-        {i, worker}
-      end
-
-    {:ok, workers}
+    workers = Enum.map(0..(@pool_size - 1), &worker_spec/1)
+    Supervisor.init(workers, strategy: :one_for_one)
   end
 
-  def handle_call({:choose, key}, _, state) do
-    worker = choose_worker(state, key)
-    {:reply, worker, state}
+  defp worker_spec(id) do
+    spec = {Todo.DbWorker, {@folder, id}}
+    Supervisor.child_spec(spec, id: id)
   end
 
-  defp choose_worker(pool, key) do
-    size = Kernel.map_size(pool)
-    i = :erlang.phash2(key, size)
-    pool[i]
+  # def handle_call({:choose, key}, _, state) do
+  #   worker = choose_worker(state, key)
+  #   {:reply, worker, state}
+  # end
+
+  defp choose_worker(key) do
+    :erlang.phash2(key, @pool_size)
   end
 end
--- lib/todo/registry.ex
+++ lib/todo/registry.ex
@@ -2,7 +2,7 @@ defmodule Todo.Registry do
   def start_link, do: Registry.start_link(name: __MODULE__, keys: :unique)
   def via_tuple(id), do: {:via, Registry, {__MODULE__, id}}
 
-  def child_spec do
+  def child_spec(_) do
     Supervisor.child_spec(Registry, id: __MODULE__, start: {__MODULE__, :start_link, []})
   end
 end
