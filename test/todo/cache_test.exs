49ee625fe49c50d7e95e8b65c1ffba52f52288c5
venir.dev <luca@venir.dev>
2024-04-11 09:43:16 +0200

    Reverted agent implementation so we can implement timeouts
    This reverts commit 8802d38d7abe76fcf432c6f2aca49bd886caa337.

--- test/todo/cache_test.exs
+++ test/todo/cache_test.exs
@@ -2,7 +2,7 @@ defmodule Todo.CacheTest do
   use ExUnit.Case
 
   test "cache get" do
-    Todo.System.start_link()
+    Todo.Registry.start_link()
     {:ok, _} = Todo.Cache.start_link(nil)
     bob_pid = Todo.Cache.get("bob")
 
