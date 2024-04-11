1ed14a1e4a53f958e3c2cae8c9564505ed64c048
venir.dev <luca@venir.dev>
2024-04-12 00:43:11 +0200

    some fancy plugins

--- mix.exs
+++ mix.exs
@@ -57,7 +57,9 @@ defmodule Hello.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:styler, "~> 0.11", only: [:dev, :test], runtime: false}
     ]
   end
 
