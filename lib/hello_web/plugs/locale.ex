ee3a7310b0f195e70d77b0187d7b564ddd281d37
venir.dev <luca@venir.dev>
2024-04-19 11:14:39 +0200

    localization experiment with a plug
    plug experiments

---
+++ lib/hello_web/plugs/locale.ex
@@ -0,0 +1,18 @@
+defmodule HelloWeb.Plugs.Locale do
+  @moduledoc false
+  import Plug.Conn
+
+  @locales ["en", "it"]
+
+  def init(default) do
+    default
+  end
+
+  def call(%Plug.Conn{params: %{"locale" => loc}} = conn, _default) when loc in @locales do
+    assign(conn, :locale, loc)
+  end
+
+  def call(conn, default) do
+    assign(conn, :locale, default)
+  end
+end
