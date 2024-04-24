383765c16a2d67ee686c8cde6ed7f9e20a6cf376
venir.dev <luca@venir.dev>
2024-04-24 12:22:31 +0200

    reset

--- lib/hello_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule HelloWeb.PageController do
-  use HelloWeb, :controller
-
-  def home(conn, _params) do
-    conn
-    |> put_flash(:error, "Let's pretend we have an error.")
-    |> redirect(to: ~p"/hello/home")
-  end
-end
--- lib/hello_web/controllers/page_json.ex
+++
@@ -1,5 +0,0 @@
-defmodule HelloWeb.PageJSON do
-  def home(_assigns) do
-    %{message: "I tried it with JSON!"}
-  end
-end
